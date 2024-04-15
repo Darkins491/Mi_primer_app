0ffe3e88d0a0be49f23ead5b325c7b9082d60607
Darkins491 <167142176+Darkins491@users.noreply.github.com>
2024-04-15 18:26:42 -0500

    ñam ñam

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ st.title("Mi primera aplicación en la Web")
 nombre = st.text_input("¿Cuál es tu nombre?")
 
 if nombre is not None:
-    st.write(f"Bienvenido, {nombre}, a mi primera app Web")
+    st.write(f"Bienvenido a mi primera app Web, ñam ñam")
     
